5c293cc3c7a9506bbe7267c743b59fde0fc8b805
Afifa Nowshin Joya <87867704+afifa5@users.noreply.github.com>
2021-07-24 22:36:21 +0600

    Initialize

--- MultivendorWebViewer/Models/MultivendorModel.cs
+++ MultivendorWebViewer/Models/MultivendorModel.cs
@@ -37,13 +37,29 @@ namespace MultivendorWebViewer.Models
             InitializeCategory(modelBuilder);
             InitializeCategoryImage(modelBuilder);
             InitializeCategoryNode(modelBuilder);
+            InitializeImage(modelBuilder);
+            InitializeNode(modelBuilder);
+            InitializeNodeImage(modelBuilder);
+            InitializeOrder(modelBuilder);
+            InitializePriceAvailability(modelBuilder);
+            InitializeProduct(modelBuilder);
+            InitializeProductImage(modelBuilder);
+            InitializeProductNode(modelBuilder);
+            InitializeProductSpecification(modelBuilder);
+            InitializeSpecification(modelBuilder);
+            InitializeSpecificationType(modelBuilder);
+            InitializeSubNode(modelBuilder);
+            InitializeText(modelBuilder);
+            InitializeTextTranslation(modelBuilder);
+            InitializeText(modelBuilder);
+
         }
 
         protected virtual void InitializeCategory(DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().ToTable("Category");
             modelBuilder.Entity<Category>().HasMany<CategoryNode>(i => i.CategoryNodes);
-            
+
             modelBuilder.Entity<Category>().HasMany<CategoryImage>(i => i.CategoryImages);
             modelBuilder.Entity<Category>().HasOptional<Text>(i => i.Name);
             modelBuilder.Entity<Category>().HasOptional<Text>(i => i.Description);
@@ -73,6 +89,156 @@ namespace MultivendorWebViewer.Models
             modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
         }
 
+        protected virtual void InitializeImage(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Image>().ToTable("Image");
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+
+      
+        protected virtual void InitializeNode(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Node>().ToTable("Node");
+            modelBuilder.Entity<Node>().HasRequired<Text>(i => i.Name);
+            modelBuilder.Entity<Node>().HasRequired<Text>(i => i.Description);
+
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+
+        protected virtual void InitializeNodeImage(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<NodeImage>().ToTable("NodeImage");
+            modelBuilder.Entity<NodeImage>().HasRequired<Image>(i => i.Image);
+            modelBuilder.Entity<NodeImage>().HasRequired<Node>(i => i.Node);
+
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+
+        protected virtual void InitializeOrder(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<Order>().HasRequired<Product>(i => i.Product);
+            modelBuilder.Entity<Order>().HasRequired<User>(i => i.User);
+
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+
+        protected virtual void InitializePriceAvailability(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PriceAvailability>().ToTable("PriceAvailability");
+            modelBuilder.Entity<PriceAvailability>().HasRequired<Product>(i => i.Product);
+            modelBuilder.Entity<PriceAvailability>().HasRequired<User>(i => i.User);
+
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+        }
+
+        protected virtual void InitializeProduct(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Product");
+            modelBuilder.Entity<Product>().HasRequired<Text>(i => i.Name);
+            modelBuilder.Entity<Product>().HasRequired<Text>(i => i.Description);
+
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+        }
+
+        protected virtual void InitializeProductImage(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductImage>().ToTable("ProductImage");
+            modelBuilder.Entity<ProductImage>().HasRequired<Image>(i => i.Image);
+            modelBuilder.Entity<ProductImage>().HasRequired<Product>(i => i.Product);
+
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+        }
+
+        protected virtual void InitializeProductNode(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductNode>().ToTable("ProductNode");
+            modelBuilder.Entity<ProductNode>().HasRequired<Node>(i => i.Node);
+            modelBuilder.Entity<ProductNode>().HasRequired<Product>(i => i.Product);
+
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+        }
+
+        protected virtual void InitializeProductSpecification(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductSpecification>().ToTable("ProductSpecification");
+            modelBuilder.Entity<ProductSpecification>().HasRequired<Product>(i => i.Product);
+            modelBuilder.Entity<ProductSpecification>().HasRequired<Specification>(i => i.Specification);
+
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+        }
+
+        protected virtual void InitializeSpecification(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Specification>().ToTable("Specification");
+            modelBuilder.Entity<Specification>().HasRequired<SpecificationType>(i => i.SpecificationType);
+            modelBuilder.Entity<Specification>().HasRequired<Text>(i => i.SpecificationText);
+
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+        }
+
+        protected virtual void InitializeSpecificationType(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SpecificationType>().ToTable("SpecificationType");
+            modelBuilder.Entity<SpecificationType>().HasRequired<Text>(i => i.SpecificationTypeText);
+
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+        }
+
+        protected virtual void InitializeSubNode(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SubNode>().ToTable("SubNode");
+            modelBuilder.Entity<SubNode>().HasRequired<Node>(i => i.Node);
+            modelBuilder.Entity<SubNode>().HasRequired<Node>(i => i.SubNodeItem);
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+        }
+
+        protected virtual void InitializeText(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Text>().ToTable("Text");
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+        }
+
+        protected virtual void InitializeTextTranslation(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TextTranslation>().ToTable("TextTranslation");
+            modelBuilder.Entity<TextTranslation>().HasRequired<Text>(i => i.Text);
+
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+        }
+
+        protected virtual void InitializeUser(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+           
+            modelBuilder.Entity<Category>().HasKey<int>(i => i.Id).Property(i => i.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+        }
+
+
     }
 
-}
+} 
